98c032be0e1e5c5b802fcbc4e9699dd7aea44546
Romaric250 <lonfonyuyromeo@gmail.com>
2024-03-05 10:54:42 +0100

    more on project setup

--- eventplanner/app/page.tsx
+++ eventplanner/app/page.tsx
@@ -1,8 +1,9 @@
+import React from "react"
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
-            <h1> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, officiis.</h2>
+            <h1> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, officiis.</h1>
         </div>
     )
 }
